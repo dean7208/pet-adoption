92acbd38eb80111da5ecc95700fbb88bf9bd8343
dean7208 <54290738+dean7208@users.noreply.github.com>
2024-02-27 08:38:09 -0500

    Finished pet filter

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ async function petsArea() {
   petsData.forEach(pet => {
     const clone = template.content.cloneNode(true);
 
+    clone.querySelector(".pet-card").dataset.species = pet.species;
     clone.querySelector("h3").textContent = pet.name;
     clone.querySelector(".pet-description").textContent = pet.description;
     clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear);
@@ -59,7 +60,17 @@ allButtons.forEach(el => {
 function handleButtonClick(e) {
   // remove active class from all buttons
   allButtons.forEach(el => el.classList.remove("active"));
+
   // add active class to specific button that just got clicked
   e.target.classList.add("active");
+
   //actually filter the pets down below
+  const currentFilter = e.target.dataset.filter;
+  document.querySelectorAll(".pet-card").forEach(el => {
+    if (currentFilter == el.dataset.species || currentFilter == "all") {
+      el.style.display = "grid";
+    } else {
+      el.style.display = "none";
+    }
+  });
 }
